f630c343974edca0f233c220fc91d4df9c1dc7d0
HuntSky <huntdev12@gmail.com>
2026-08-18 21:07:39 +0530

    feat(stories):fix

--- blog_final_fixed_links/writings/dilkashi/progress.js
+++
@@ -1,8 +0,0 @@
-const bar = document.querySelector('.chapter-progress span');
-const update = () => {
-  const max = document.documentElement.scrollHeight - window.innerHeight;
-  bar.style.width = (max > 0 ? (window.scrollY / max) * 100 : 0) + '%';
-};
-window.addEventListener('scroll', update, {passive:true});
-window.addEventListener('resize', update);
-update();
